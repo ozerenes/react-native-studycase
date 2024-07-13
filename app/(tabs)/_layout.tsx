c0e18a7d3ee92550e64e78b6c525b427002fc11a
eozer <eozer@yahoo.com>
2024-07-13 13:55:20 +0300

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,12 +7,11 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={25} {...props} />;
 }
 
 export default function TabLayout() {
--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -9,8 +9,6 @@ export function ExternalLink(
   return (
     <Link
       target="_blank"
-      {...props}
-      // @ts-expect-error: External URLs are not typed.
       href={props.href}
       onPress={(e) => {
         if (Platform.OS !== 'web') {
